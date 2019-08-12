cc2bbb4863698cd1fbf4c96a8da58583cc2f3c53
zhoujf <zhoujf1636@163.com>
2019-08-12 11:31:40 +0800

    Initial Commit

---
+++ abstract_base_class/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  abstract_base_class
+//
+//  Created by JF Zhou on 2019/8/12.
+//  Copyright © 2019 JF Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
